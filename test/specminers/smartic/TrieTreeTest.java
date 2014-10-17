3214553b8731b0026c57cd58d9e4171358aba507
otmar <otmar@OptiPlex-7010>
2014-10-17 09:50:24 -0300

    Arrumando classpath para teste da árvore Trie funcionar

--- test/specminers/smartic/TrieTreeTest.java
+++ test/specminers/smartic/TrieTreeTest.java
@@ -10,7 +10,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import jp.ac.titech.cs.se.sparesort.MiningStrategy;
 import jp.ac.titech.cs.se.sparesort.SequenceDatabase;
+import jp.ac.titech.cs.se.sparesort.bide.RecursiveBIDE;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -63,11 +65,14 @@ public class TrieTreeTest {
         
         SequenceDatabase<String> seqDB = new SequenceDatabase<>();
 
+        MiningStrategy<String> st = new RecursiveBIDE<>();
+        seqDB.setMiningStrategy(st);
+        
         for (Trace t : traces) {
             seqDB.addSequence(t.getEvents());
         }
         
-        Map<List<String>, Integer> closedSeqs = seqDB.mineFrequentClosedSequences(1);
+        Map<List<String>, Integer> closedSeqs = seqDB.mineFrequentClosedSequences(2);
         
         TrieTree tree = new TrieTree(sequences, closedSeqs);
         
