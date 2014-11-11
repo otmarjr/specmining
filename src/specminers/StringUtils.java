db3e05e743c29fa83e317cdaa81e9d9cf4330624
otmar <otmar@OptiPlex-7010>
2014-11-11 17:20:08 -0200

    Passando no teste do capítulo 5 do David Lo.

--- src/specminers/StringUtils.java
+++ src/specminers/StringUtils.java
@@ -5,13 +5,13 @@
  */
 package specminers;
 
+import simpleSequitur.sequitur;
 import edu.hawaii.jmotif.gi.GrammarRuleRecord;
 import java.util.List;
 import edu.hawaii.jmotif.gi.sequitur.SAXRule;
 import edu.hawaii.jmotif.gi.sequitur.SequiturFactory;
 import edu.hawaii.jmotif.timeseries.TSException;
 import java.util.ArrayList;
-
 
 /**
  *
@@ -20,20 +20,39 @@ import java.util.ArrayList;
 public class StringUtils {
     public static String generateRegexToMatchInput(String input) 
             throws TSException{
-        String regex = null;
         
-        SAXRule r = SequiturFactory.runSequitur(input);
-
-        ArrayList<GrammarRuleRecord> recs = r.getRuleRecords();
+        return sequitur.getGrammarBasedRegex(input, null);
+    }
+    
+    public static int getSequenceAlignmentPenalty(String s1, String s2, int gapPenalty, int mismatchPenalty){
+        int m = s1.length();
+        int n = s2.length();
         
-        String result = "";
+        int[][] A = new int[m+1][n+1];
         
-        for (GrammarRuleRecord gr : recs){
-            result += "-" + gr.getRuleName() + " : " + gr.getRuleString() +" - exp: " + gr.getExpandedRuleString();
+        for (int i=0;i<m;i++) A[i][0] = 0;
+        for (int i=0;i<n;i++) A[0][i] = 0;
+        
+        for (int i=1;i<=m;i++){
+            for (int j=1;j<=n;j++){
+                String s1i = s1.substring(i-1,i);
+                String s2j = s2.substring(j-1,j);
+                
+                int previousPenalty = A[i][j] = A[i-1][j-1];
+                
+                if (s1i.equals(s2j)){
+                    A[i][j] = previousPenalty;
+                }
+                else{
+                    int p1 = previousPenalty + mismatchPenalty;
+                    int p2 = gapPenalty + A[i-1][j];
+                    int p3 = gapPenalty + A[i][j-1];
+                    
+                    A[i][j] = Math.min(Math.min(p1, p2), p3);
+                }
+            }
         }
         
-        return result;
-        
-        // return regex;
+        return A[m][n];
     }
 }
