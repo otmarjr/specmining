218d1b63b97be4f8367743cdaee2cdf3e48d4bc7
otmar <otmar@OptiPlex-7010>
2014-11-24 23:20:00 -0200

    LearningBlock finished!

--- src/specminers/smartic/LearningBlock.java
+++ src/specminers/smartic/LearningBlock.java
@@ -9,25 +9,23 @@ import java.util.List;
 import cz.cuni.mff.ksi.jinfer.base.automaton.*;
 import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.skstrings.SKStrings;
 import java.util.stream.Collectors;
+
 /**
  *
  * @author otmarpereira
  */
 public class LearningBlock {
 
-    public static List<List<String>> learnAutomaton(List<Trace> traces) throws InterruptedException {
-        List<List<String>> transitions = null;
-
+    public static Automaton<String> learnAutomaton(List<Trace> traces, int k, double s) throws InterruptedException {
         Automaton<String> aut = new Automaton<>(true);
-        
+
         traces.forEach(t -> aut.buildPTAOnSymbol(t.getEvents()));
-        
+
         SKStrings<String> alg;
-        alg = new SKStrings<>(2, 1f,"AND");
-        
-        List<List<List<State<String>>>> merge = alg.getMergableStates(aut);
-        
-        String s = aut.toString();
-        return transitions;
+        alg = new SKStrings<>(k, s, "AND");
+
+        alg.mergeStates(aut);
+
+        return aut;
     }
 }
