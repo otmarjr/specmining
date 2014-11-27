450f3701a6db007de11dd42b80c86336d8b5f80a
otmarjr <otmarjr@gmail.com>
2014-11-27 20:56:40 -0200

    Merge origin/master
    Conflicts:
    	lib/cz-cuni-mff-ksi-jinfer-base.jar
    	lib/cz-cuni-mff-ksi-jinfer-twostep.jar

--- src/specminers/smartic/MergingBlock.java
+++ src/specminers/smartic/MergingBlock.java
@@ -8,6 +8,7 @@ package specminers.smartic;
 import cz.cuni.mff.ksi.jinfer.base.automaton.Automaton;
 import cz.cuni.mff.ksi.jinfer.base.automaton.State;
 import cz.cuni.mff.ksi.jinfer.base.automaton.Step;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,6 +16,29 @@ import java.util.List;
  * @author otmarpereira
  */
 public class MergingBlock {
+    
+    class TransitionString{
+
+        TransitionString(Step<String> step, Automaton<String> automaton) {
+            loadTokens();
+            loadProbabilities();
+            this.initialStep = step;
+        }
+        
+        List<String> tokens;
+        List<Double> probabilities;
+        Step<String> initialStep;
+        Automaton<String> automaton;
+        
+        private void loadTokens() {
+            this.tokens = new LinkedList<>();
+        }
+
+        private void loadProbabilities() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
     Automaton<String> X;
     Automaton<String> Y;
     Automaton<String> mergedAutomaton;
@@ -25,7 +49,19 @@ public class MergingBlock {
         this.mergedAutomaton = null;
     }
     
+    public boolean sourceNodesShareSuffixProbabilities(Step<String> s1, Step<String> s2){
+        return false;
+    }
+    
+    public boolean transitionsAreEquivalent(Step<String> s1, Step<String> s2){
+        return false;
+    }
+    
     public List<Step<String>> getPrefix(Automaton<String> aut, State<String> n){
+        return null;
+    }
+    
+    public List<Step<String>> getSuffixes(Automaton<String> aut, State<String> n){
         return null;
     }
     
